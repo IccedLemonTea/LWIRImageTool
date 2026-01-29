46d13389f080311237287fb3b9f47861b30a1eb7
Cooper White <cjw9009@g.rit.edu>
2026-01-28 19:51:56 -0500

    refactored to include pydantic and new config file

--- LWIRImageTool/ImageDataFactory.py
+++ LWIRImageTool/ImageDataFactory.py
@@ -1,33 +1,30 @@
-### ImageDataFactory Class ###
-# Author : Cooper White
-# Date : 09/30/2025
-# File : ImageDataFactory.py
-
-import LWIRImageTool
-from typing import Literal
-
-ImageFormat = Literal["envi", "rjpeg"]
+from .ImageDataConfig import ImageDataConfig
+from .ENVI import ENVI
+from .RJPEG import RJPEG
 
 
-class ImageDataFactory(object):
+class ImageDataFactory:
     """Factory for creating source-agnostic ImageData objects."""
 
     @staticmethod
-    def create_from_file(
-        filename: str,
-        fileformat: ImageFormat = "envi",
-        bitdepth: int = 16,
-    ):
-        fileformat = fileformat.lower()
+    def create_from_file(config: ImageDataConfig):
+        fileformat = config.fileformat.lower()
 
-        if not ImageDataFactory.is_valid_image_file(filename, fileformat):
-            raise ValueError(f"Invalid {fileformat} file: {filename}")
+        if not ImageDataFactory.is_valid_image_file(
+            config.filename, fileformat
+        ):
+            raise ValueError(
+                f"Invalid {fileformat} file: {config.filename}"
+            )
 
         if fileformat == "envi":
-            return LWIRImageTool.ENVI(filename, bitdepth=bitdepth)
+            return ENVI(
+                config.filename,
+                bitdepth=config.bitdepth
+            )
 
         if fileformat == "rjpeg":
-            return LWIRImageTool.RJPEG(filename)
+            return RJPEG(config.filename)
 
         raise ValueError(f"Unsupported file format: {fileformat}")
 
@@ -35,7 +32,6 @@ class ImageDataFactory(object):
     def is_valid_image_file(filename: str, fileformat: str) -> bool:
         if fileformat == "rjpeg":
             return filename.endswith("_R.jpg")
-        elif fileformat == "envi":
+        if fileformat == "envi":
             return filename.endswith(".hdr")
-        else:
-            return False
+        return False
